b2112283c24608a7c0a1bdd647f3d4a9edb48269
Andreea Acatrinei <andreea.acatrinei@tapptitude.com>
2023-11-08 16:42:40 +0200

    Add horizontal labels to bar chart

--- android/src/main/java/com/tappchart/android/MainActivity.kt
+++ android/src/main/java/com/tappchart/android/MainActivity.kt
@@ -3,7 +3,6 @@ package com.tappchart.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,9 +11,9 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tappchart.bar.BarChart
 import com.tappchart.bar.model.Bar
-import com.tapptitude.tappchart.HelloWorld
 import com.tapptitude.tappchart.model.AxisInterval
 import com.tapptitude.tappchart.model.DataLabel
 import com.tapptitude.tappchart.model.ValueLabel
@@ -26,24 +25,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+//            BarChart(
+//                modifier = Modifier.fillMaxWidth().height(300.dp).padding(24.dp),
+//                yAxisInterval = AxisInterval(8f,0f),
+//                data = listOf(
+//                    Bar(DataLabel("Guest"),1.3f),
+//                    Bar(DataLabel("New User"),3f),
+//                    Bar(DataLabel("Existing User"),8f)
+//                ),
+//                verticalLabel = listOf(
+//                    ValueLabel("0",0f),
+//                    ValueLabel("2K",2f),
+//                    ValueLabel("4K",4f),
+//                    ValueLabel("6K",6f)
+//                ),
+//                labelStyle = TextStyle.Default,
+//                spacingBetweenBars = 16.dp,
+//                axisStyle = AxisStyle(1.dp, Brush.verticalGradient(listOf(Color.Gray, Color.Gray))),
+//                barColor = listOf(Color.Blue, Color.Blue, Color.Blue)
+//            )
+
             BarChart(
                 modifier = Modifier.fillMaxWidth().height(300.dp).padding(24.dp),
-                yAxisInterval = AxisInterval(8f,0f),
+                yAxisInterval = AxisInterval(28f,0f),
                 data = listOf(
-                    Bar(DataLabel("Guest"),1.3f),
-                    Bar(DataLabel("New User"),3f),
-                    Bar(DataLabel("Existing User"),8f)
+                    Bar(DataLabel("Apr 2020"),22f),
+                    Bar(DataLabel("May 2020"),21.5f),
+                    Bar(DataLabel("Jun 2020"),23.5f),
+                    Bar(DataLabel("Jul 2020"),26f),
+                    Bar(DataLabel("Aug 2020"),22f),
+                    Bar(DataLabel("Sep 2020"),21.5f)
                 ),
                 verticalLabel = listOf(
                     ValueLabel("0",0f),
-                    ValueLabel("2K",2f),
-                    ValueLabel("4K",4f),
-                    ValueLabel("6K",6f)
+                    ValueLabel("8K",8f),
+                    ValueLabel("16K",16f),
+                    ValueLabel("24K",24f)
                 ),
-                labelStyle = TextStyle.Default,
-                spacingBetweenBars = 16.dp,
+                labelStyle = TextStyle.Default.copy(fontSize = 10.sp),
+                spacingBetweenBars = 8.dp,
                 axisStyle = AxisStyle(1.dp, Brush.verticalGradient(listOf(Color.Gray, Color.Gray))),
-                barColor = listOf(Color.Blue, Color.Blue, Color.Blue)
+                barColor = listOf(Color.Blue),
+                yAxisPaddingToFirstBar = 32.dp,
             )
         }
     }
--- shared/bar/src/commonMain/kotlin/com/tappchart/bar/BarChart.kt
+++ shared/bar/src/commonMain/kotlin/com/tappchart/bar/BarChart.kt
@@ -3,15 +3,12 @@
 package com.tappchart.bar
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
@@ -35,45 +32,67 @@ fun BarChart(
     axisStyle: AxisStyle,
     background: List<BackgroundStyle> = listOf(),
     barColor: List<Color>,
-    yAxisPadding:Dp = 24.dp
+    yAxisPaddingToFirstBar: Dp = 24.dp,
+    yAxisPaddingToLine: Dp = 8.dp,
+    xAxisPaddingToBars:Dp = 2.dp
 ) {
 
     val textMeasurer = rememberTextMeasurer()
     Canvas(modifier = modifier) {
-        drawYAxis(verticalLabel, yAxisInterval, textMeasurer)
 
-        val startBarsPadding = yAxisPadding.toPx()
-        var barTopLeft = startBarsPadding
-        val barWidth = (size.width - (spacingBetweenBars.toPx() * (data.size)) - startBarsPadding) / data.size
+        //region vertical labels
+        val firstLabelSize = textMeasurer.measure(verticalLabel.first().label).size
+        val firstLabelPosition = size.height - firstLabelSize.height/2
+        val maxWidth = verticalLabel.maxOf { textMeasurer.measure(it.label).size.width }.toFloat()
+
+        var bottomOfChart = size.height
+        verticalLabel.forEachIndexed { index, label ->
+
+            val labelPosition = firstLabelPosition - (size.height * label.yValue) / yAxisInterval.max
+            val labelSize = textMeasurer.measure(label.label, labelStyle)
+            drawText(
+                textLayoutResult = labelSize,
+                topLeft = Offset(maxWidth - labelSize.size.width, labelPosition)
+            )
+            val lineY = labelPosition + labelSize.size.height / 2
+            if (index == 0) bottomOfChart = lineY
+            val startLine = maxWidth + yAxisPaddingToLine.toPx()
+            drawLine(
+                color = Color.Gray,
+                start = Offset(startLine, lineY),
+                end = Offset(size.width, lineY)
+            )
+        }
+        //endregion
+
+        //region bars
+        val startBarsPadding = yAxisPaddingToFirstBar.toPx()
+        var barTopLeftPosition = startBarsPadding
+        val barsCount = data.size
+        val barWidth = (size.width - spacingBetweenBars.toPx() * barsCount - startBarsPadding) / barsCount
+        val differenceOfHeightOfCanvasAndFirstLabel = size.height - bottomOfChart
         data.forEachIndexed { index, bar ->
-            val color = barColor[index]
-
+            val color = barColor.getOrNull(index) ?: barColor.first()
             drawRect(
                 color = color,
                 topLeft = Offset(
-                    x = barTopLeft,
+                    x = barTopLeftPosition,
                     y = size.height - (bar.value * size.height) / yAxisInterval.max
                 ),
-                size = Size(barWidth, (bar.value * size.height) / yAxisInterval.max)
+                size = Size(
+                    barWidth,
+                    (bar.value * size.height) / yAxisInterval.max - differenceOfHeightOfCanvasAndFirstLabel
+                )
             )
-            barTopLeft += barWidth + spacingBetweenBars.toPx()
-        }
-    }
-}
+            barTopLeftPosition += barWidth + spacingBetweenBars.toPx()
 
-fun DrawScope.drawYAxis(
-    verticalLabel: List<ValueLabel>,
-    yAxisInterval: AxisInterval,
-    textMeasurer: TextMeasurer
-) {
-    val firstLabelPosition =
-        size.height - textMeasurer.measure(verticalLabel.first().label).size.height
-    verticalLabel.forEach { label ->
-        val newPos = firstLabelPosition - (size.height * label.yValue) / yAxisInterval.max
-        drawText(
-            textMeasurer = textMeasurer,
-            text = label.label,
-            topLeft = Offset(0f, newPos)
-        )
+            //xAxis labels
+            val horizontalLabelSize = textMeasurer.measure(bar.key.label, labelStyle)
+            drawText(
+                textLayoutResult = horizontalLabelSize,
+                topLeft = Offset(barTopLeftPosition - barWidth - spacingBetweenBars.toPx(), size.height + xAxisPaddingToBars.toPx())
+            )
+        }
+        //endregion
     }
 }
